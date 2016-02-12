834956de93770fc2c516b0d762f56f4da3b0deaa
pau1m <paul@citywebconsultants.co.uk>
2016-02-12 14:38:38 +0000

    First commit
    Adding user tests and beginning refactor
    
    Renaming file and a little refactoring
    
    Tidying up a little
    
    Creating readme

---
+++ iris_core/test/test_config.js
@@ -0,0 +1,20 @@
+
+
+
+
+var exports = {};
+
+exports.adminUser = {
+    login: {
+        username: "foo",
+        password: "foo"
+    },
+    auth: {
+        token: "",
+        userid: "1"
+    }
+};
+
+exports.baseURL = 'http://www.iris.local:' + '4000';
+
+module.exports = exports
